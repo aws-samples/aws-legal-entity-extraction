203a9fdfb6dc142dfe704214ac3980177744d8a1
Raj Pathak <73082957+rppth@users.noreply.github.com>
2022-02-04 21:27:08 -0600

    Add files via upload

--- bucketnamechange.py
+++ bucketnamechange.py
@@ -1,10 +1,10 @@
 bucketname = input(" Enter the name of your bucket: ")
 inputted_s3_uri = f"s3://{bucketname}"
-replace_with_input_source = "s3://comprehend-semi-structured-documents-us-east-1-accountnumber/source-semi-structured-documents/"
-input_source = f"{inputted_s3_uri}/source/"
+replace_with_input_source = "s3://legal-entity-blog"
+input_source = f"{inputted_s3_uri}"
 
-replace_with_input_annotations = "s3://comprehend-semi-structured-documents-us-east-1-accountnumber/output/legal-entity-label-job-labeling-job-20220104T172242/annotations/consolidated-annotation/consolidation-response/iteration-1/"
-input_annotations =  f"{inputted_s3_uri}/"
+replace_with_input_annotations = "s3://legal-entity-blog"
+input_annotations =  f"{inputted_s3_uri}"
 
 with open("output.manifest") as output_file:
     data = output_file.read()
